985858f38ec66cb1ddf9f13cdb952217c084e616
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-11-24 17:04:35 +0530

    chore: add images and gh-pages to deploy

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import singleSpaSvelte from 'single-spa-svelte';
 import App from './App.svelte';
 
-/*
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
-
-export default app;
-*/
-
 const svelteLifecycles = singleSpaSvelte({
   component: App,
   domElementGetter: () => document.getElementById('single-spa-application:@feedme/restaurants'),
